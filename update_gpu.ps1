a9cf20ca7c0e6650d78e1a40256f260feebb181d
Alec-afana <95040550+Alec-afana@users.noreply.github.com>
2025-10-25 16:17:43 +0300

    Add files via upload

---
+++ update_gpu.ps1
@@ -0,0 +1,16 @@
+﻿Write-Host "Определяем GPU..."
+$gpus = Get-WmiObject Win32_VideoController | Select-Object Name, DriverVersion, DriverDate
+$gpus | Format-Table
+
+foreach ($gpu in $gpus) {
+    if ($gpu.Name -like "*NVIDIA*") {
+        Write-Host "Скачиваем NVIDIA драйвер..."
+        Start-Process "https://www.nvidia.com/Download/index.aspx?lang=ru"
+    }
+    elseif ($gpu.Name -like "*AMD*" -or $gpu.Name -like "*Radeon*") {
+        Write-Host "Скачиваем AMD драйвер..."
+        Start-Process "https://www.amd.com/ru/support"
+    }
+}
+
+Write-Host "`n После загрузки установи драйверы и перезагрузи компьютер."
