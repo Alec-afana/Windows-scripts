8ca313109a420439993dc92375e6cd7b82675f98
Alec-afana <95040550+Alec-afana@users.noreply.github.com>
2025-10-25 16:07:45 +0300

    Add files via upload

---
+++ bsod-error.ps1
@@ -0,0 +1,12 @@
+﻿# === Поиск BSOD-ошибок VIDEO_MEMORY_MANAGEMENT_INTERNAL ===
+
+Write-Host "=== Проверка, были ли ошибки dxgmms2.sys / VIDEO_MEMORY_MANAGEMENT_INTERNAL ===`n"
+
+Get-WinEvent -FilterHashtable @{
+    LogName = 'System'
+    ProviderName = 'Microsoft-Windows-WER-SystemErrorReporting'
+    StartTime = (Get-Date).AddDays(-7)
+} | Where-Object {
+    $_.Message -match "VIDEO_MEMORY_MANAGEMENT_INTERNAL" -or
+    $_.Message -match "dxgmms2.sys"
+} | Select TimeCreated, Message -First 10 | Format-List
